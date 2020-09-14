9d29a6fc54b1d587246088b7011a44f7fc892ec7
Rushikesh Deshmukh <49672675+Mrx207@users.noreply.github.com>
2020-09-14 10:36:08 +0530

    Added all files

---
+++ dice.js
@@ -0,0 +1,10 @@
+var score,round,actPlayer,dice;
+
+var score=[0,0];
+round=0;
+actPlayer=0;
+
+dice = Math.floor(Math.random() *6)+1;
+document.querySelector('#current-'+ actPlayer).textContent=dice;
+
+document.querySelector()
